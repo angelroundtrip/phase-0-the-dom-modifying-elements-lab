8f38f9f364bf35ab0b45cbbcb4ad122a4ed0ba03
Angel Rosario <arosario.us@gmail.com>
2021-12-02 20:06:18 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+document.querySelector('main')
+main.remove();
+
+
+const newHeader = document.createElement("h1");
+  newHeader.h1 = "victory";
+
+
+newHeader.id = "victory";
+
+
+newHeader.textContent = "Angel is the champion";
+document.body.append(newHeader);
